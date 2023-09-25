1bae62a43ba90344f2c2a2fd1e0846634d9998bd
Marko Lukic <marko.lukic.12321@gmail.com>
2023-09-26 00:32:37 +0200

    generators._random: Add a non-end and self-loop bias to random generators (~6600)

--- src/wordchain/generators/_random.py
+++ src/wordchain/generators/_random.py
@@ -13,7 +13,16 @@ __all__ = 'RandomGenerator', 'WeightedRandomGenerator'
 
 class RandomGenerator(_base.StepByStepGenerator):
     def _pick_next_word(self, graph: 'Graph', node: 'Node') -> str:
-        return random.choice(tuple(node.transitions))
+        end_word: str | None = None
+        non_end_words: list[str] = []
+        for word, next_node in node.transitions.items():
+            if word == node.value:
+                return word
+            if next_node is graph.end:
+                end_word = word
+            else:
+                non_end_words.append(word)
+        return random.choice(non_end_words) if len(non_end_words) > 0 else end_word
 
 
 class WeightedRandomGenerator(_base.StepByStepGenerator):
@@ -25,9 +34,20 @@ class WeightedRandomGenerator(_base.StepByStepGenerator):
         super().__init__()
 
     def _pick_next_word(self, graph: 'Graph', node: 'Node') -> str:
-        scores = tuple(self._evaluator.get_score(next_node) + 1  # avoid having all 0s
-                       for next_node in node.transitions.values())
-        return random.choices(tuple(node.transitions), weights=scores)[0]
+        end_word: str | None = None
+        non_end_words: list[str] = []
+        non_end_scores: list[float | int] = []
+        for word, next_node in node.transitions.items():
+            if word == node.value:
+                return word
+            if next_node is graph.end:
+                end_word = word
+            else:
+                non_end_words.append(word)
+                non_end_scores.append(self._evaluator.get_score(next_node))
+        chosen_word = (random.choices(non_end_words, weights=non_end_scores)[0]
+                       if len(non_end_words) > 0 else end_word)
+        return chosen_word
 
     def generate(self, graph: 'Graph') -> tuple[str]:
         self._evaluator.prepare(graph)
